c449d5d9ea352813b1cc19ba3bfe1336da88d3fc
Inda <indadev@users.noreply.github.com>
2020-07-07 21:31:09 +0200

    Update GameController.java
    Support list of games

--- back-end/src/main/java/com/indadev/rps/GameController.java
+++ back-end/src/main/java/com/indadev/rps/GameController.java
@@ -5,30 +5,42 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 
 
 @RestController
 public class GameController {
 
-    Game currentGame = null;
+    ArrayList<Game> games = null;
 
     @GetMapping("/game/new")
     public String createNewGame(){
         //Generate game id
         String generatedString = RandomStringUtils.randomAlphanumeric(10);
 
-        currentGame = new Game(generatedString);
-        return "New game id " + generatedString;
+        if (games == null){
+            games = new ArrayList<Game>();
+        }
+        games.add(new Game(generatedString));
+        return games.toString();
     }
 
     @GetMapping("/game")
-    public Game gameStatus(){
-        return currentGame;
+    public String gameStatus(){
+
+        return games.toString();
     }
 
     @GetMapping("/game/play")
-    public Game playGame(){
-        currentGame.play();
-        return currentGame;
+    public String playGame(@RequestParam(value = "id", defaultValue = "") String id){
+
+        Game currentGame = null;
+        for (Game g: games){
+            if (g.getId().equalsIgnoreCase(id)){
+                g.play();
+                currentGame = g;
+            }
+        }
+        return currentGame.toString();
     }
 }
